d046c89a6aaa6cd66a9b146b6b03a93b0bb883af
wathek-alyousefi <wathekalyousefi@gmail.com>
2024-04-29 21:03:57 +0300

    b

--- app/Http/Controllers/dashboardController.php
+++ app/Http/Controllers/dashboardController.php
@@ -4,7 +4,7 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 
-class dashboardController extends Controller
+class dashBoardController extends Controller
 {
     function index() {
         $j = true;
